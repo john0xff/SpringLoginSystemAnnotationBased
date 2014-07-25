fb2979e771f2e980b6c93d2a13b61ce4412acfb0
BartBien <phoenixjcam@gmail.com>
2014-07-26 00:40:02 +0200

    pages login, join, admin - annotation based

---
+++ src/main/java/com/phoenixjcam/config/core/SpringMvcInitializer.java
@@ -0,0 +1,30 @@
+package com.phoenixjcam.config.core;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.phoenixjcam.config.AppConfig;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses()
+	{
+		return new Class[]
+		{ AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses()
+	{
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings()
+	{
+		return new String[]
+		{ "/" };
+	}
+
+}
---
+++ src/main/java/com/phoenixjcam/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.phoenixjcam.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer
+{
+	// do nothing
+}
